082f9a36204ac390682672ae606854172199fc09
Luca5Eckert <ecklukatv@gmail.com>
2025-12-03 16:32:47 -0300

    feat: adiciona padrão de modulo

---
+++ patterns/module_pattern/ModulePattern.js
@@ -0,0 +1,36 @@
+const bankAccount = function() {
+    let balance = 0;
+
+    function changeBalance(amount){
+        balance += amount;
+    }
+
+    const deposit = (amount) => {
+        changeBalance(amount);
+        console.log("Depositado");
+    }
+    
+
+    const withdraw = (amount) => {
+        if(amount > balance){
+            console.log("Saldo insuficiente");
+            return;
+        }
+        changeBalance(-amount);
+        console.log(`Sacado ${amount}`);
+    }
+
+    const getBalance = () => {
+        return balance;
+    }
+
+    return {
+        deposit: deposit,
+        withdraw: withdraw,
+        getBalance: getBalance
+    }
+
+}();
+
+bankAccount.deposit(100);
+console.log(bankAccount.balance);
